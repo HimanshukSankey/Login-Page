cb2440a0627173180a980b72a9c43c65f1935fda
HimanshukSankey <himanshu.k@sankeysolutions.com>
2024-05-08 17:04:38 +0530

    Email Validator Added

--- script.js
+++ script.js
@@ -18,36 +18,45 @@ const submitTask = (event) => {
     let passValidator = document.getElementById('inValidPasswordMessage')
     let emailChecker = document.getElementById('inValidEmailMessage')
     
-    //Regular Expression for Password Validation
-    let checkPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    //Regular Expression for Password Validation and Email Validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const checkPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 
     //Checking the Password with .test( ) method
-    if (!checkPassword.test(pass)) {
-        passValidator.innerText = 'Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one digit, and one special character.';
-        return false // Prevent Submission of Form
-
-    } else {
-        passValidator.innerText = "";
-
-        //Checking if Email or Password Exist in Database
-
-        if (email !== EmailData){
-            emailChecker.innerText = "Invalid Email"
-            return false;
-        }
-        else{
-            emailChecker.innerText = "";
-            if(pass !== passData){
-                passValidator.innerText = "Invalid Password";
-                return false;
+    if(!emailRegex.test(email)){
+        emailChecker.innerText = 'Email Format must be similar as given demo@gmail.com';
+        return false;
+    }
+    else{
+        emailChecker.innerHTML ="";
+        if (!checkPassword.test(pass)) {
+            passValidator.innerText = 'Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one digit, and one special character.';
+            return false // Prevent Submission of Form
     
-            }else{
-                passValidator.innerText = "";
-                redirectToHomePage();
+        } else {
+            passValidator.innerText = "";
+    
+            //Checking if Email or Password Exist in Database
+    
+            if (email !== EmailData){
+                emailChecker.innerText = "Invalid Email"
+                return false;
             }
-        }
+            else{
+                emailChecker.innerText = "";
+                if(pass !== passData){
+                    passValidator.innerText = "Invalid Password";
+                    return false;
         
-
+                }else{
+                    passValidator.innerText = "";
+                    redirectToHomePage();
+                }
+            }
+            
+    
+        }
     }
 
 
